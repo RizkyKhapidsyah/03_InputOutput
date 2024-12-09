ef51f9879d300ef7ca7d8e2d64a94a9febd419fe
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2024-12-09 21:50:53 +0700

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	int i;
+
+	std::cout << "Masukkan Nilai                  : "; std::cin >> i;
+	std::cout << "Nilai Yang Anda Masukkan Adalah : "; std::cout << i << std::endl;
+
+	return 0;
+}
